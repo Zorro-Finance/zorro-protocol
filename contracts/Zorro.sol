36e5629f7492b0633eb8871036cc2b4edecd0a02
kristophm <k@kristophmatthews.com>
2021-12-16 08:48:52 -0500

    libs/helpers/interfaces

--- contracts/Zorro.sol
+++ contracts/Zorro.sol
@@ -1,7 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract Zorro {
-  constructor() public {
-  }
+import "./helpers/ERC20.sol";
+
+import "./libraries/Address.sol";
+
+import "./libraries/SafeERC20.sol";
+
+import "./helpers/Ownable.sol";
+
+contract Zorro is ERC20("AUTOv2", "AUTO"), Ownable {
+    function mint(address _to, uint256 _amount) public onlyOwner {
+        _mint(_to, _amount);
+    }
 }
