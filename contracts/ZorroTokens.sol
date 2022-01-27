ef03d729d1cf3ca14dea880f517f3d2ec36339aa
kristophm <k@kristophmatthews.com>
2022-01-27 09:28:30 -0500

    zUSDC minting and burning

--- contracts/ZorroTokens.sol
+++ contracts/ZorroTokens.sol
@@ -18,7 +18,19 @@ contract Zorro is ERC20("ZORRO", "ZOR"), Ownable {
 
 /// @title Zorro USD synthetic token (used for bridges)
 contract ZUSDC is ERC20("ZORRO_USDC", "ZUSDC"), Ownable {
+    /// @notice Mints ERC20 tokens
+    /// @dev Can only be called by owner
+    /// @param _to The address to send minted tokens to
+    /// @param _amount The amount of tokens to mint
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
+    }
+
+    /// @notice Burns ERC20 tokens
+    /// @dev Can only be called by owner
+    /// @param _account The address to burn tokens from
+    /// @param _amount The amount of tokens to burn
+    function burn(address _account, uint256 _amount) public onlyOwner {
+        _burn(_account, _amount);
     }
 }
