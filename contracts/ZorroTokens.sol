6bfc731c0ccfe02b8826c5fd0e100d9f0f64b8aa
kristophm <k@kristophmatthews.com>
2022-01-27 09:28:21 -0500

    zUSDC minting and burning

--- contracts/ZorroTokens.sol
+++ contracts/ZorroTokens.sol
@@ -9,7 +9,15 @@ import "./libraries/SafeERC20.sol";
 
 import "./helpers/Ownable.sol";
 
+/// @title The Zorro token (cross chain)
 contract Zorro is ERC20("ZORRO", "ZOR"), Ownable {
+    function mint(address _to, uint256 _amount) public onlyOwner {
+        _mint(_to, _amount);
+    }
+}
+
+/// @title Zorro USD synthetic token (used for bridges)
+contract ZUSDC is ERC20("ZORRO_USDC", "ZUSDC"), Ownable {
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
     }
