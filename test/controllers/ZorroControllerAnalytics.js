3bebf4ed3a87a0fdac453a1adbd0a703597257cc
Deltakilomilo <deltakilomilo@zorro.finance>
2022-05-19 10:23:28 -0400

    pending rewards refactor

--- test/controllers/ZorroControllerAnalytics.js
+++ test/controllers/ZorroControllerAnalytics.js
@@ -1,8 +1,12 @@
 const MockZorroController = artifacts.require('MockZorroController');
+const MockVaultZorro = artifacts.require("MockVaultZorro");
+const MockZorroToken = artifacts.require("MockZorroToken");
 
-const getInstance = async (accounts) => {
+const setupObj = async (accounts) => {
     // Get instance 
     const instance = await MockZorroController.deployed();
+    const vault = await MockVaultZorro.deployed();
+    const ZORToken = await MockZorroToken.deployed();
 
     // Set rewards params
     await instance.setRewardsParams(
@@ -18,11 +22,10 @@ const getInstance = async (accounts) => {
     // Add pool
     await instance.add(
         1, // alloc point (multiplier)
-        '0x0000000000000000000000000000000000000000',
-        true,
-        '0x0000000000000000000000000000000000000001'
+        ZORToken.address, // _want address
+        true, // withUpdate
+        vault.address // _vault address
     );
-    console.log('block after adding pool: ', await web3.eth.getBlockNumber());
 
     // Set Zorro Oracle (to allow setting below)
     await instance.setZorroControllerOracle(accounts[0]);
@@ -37,22 +40,20 @@ const getInstance = async (accounts) => {
         web3.utils.toBN(1e6), // _ZorroTotalVaultTVLUSD
         web3.utils.toBN(web3.utils.toWei('800000', 'ether')) // _publicPoolZORBalance
     );
-    console.log('blocksPerDay: ', (await instance.blocksPerDay.call()).toString());
-    console.log('chainmultip: ', (await instance.chainMultiplier.call()).toString());
-    console.log('bpmin: ', (await instance.ZORRODailyDistributionFactorBasisPointsMin.call()).toString());
-    console.log('bpmax: ', (await instance.ZORRODailyDistributionFactorBasisPointsMax.call()).toString());
-    console.log('baserewardratebp: ', (await instance.baseRewardRateBasisPoints.call()).toString());
-    console.log('just set ZORPerBlock: ', (await instance.ZORROPerBlock.call()).toString());
 
-    return instance;
+    return {
+        instance,
+        vault,
+        ZORToken
+    };
 };
 
 
-contract('ZorroControllerAnalytics', async accounts => {
+contract('ZorroControllerAnalytics::Pending Rewards', async accounts => {
     let instance;
 
     before(async () => {
-        instance = await getInstance(accounts);
+        instance = (await setupObj(accounts)).instance;
     });
 
     it('should return 0 pending rewards for when the pool has no contributions', async () => {
@@ -65,14 +66,6 @@ contract('ZorroControllerAnalytics', async accounts => {
             pendingRewards,
             0
         );
-    });
-});
-
-contract('ZorroControllerAnalytics', async accounts => {
-    let instance;
-
-    before(async () => {
-        instance = await getInstance(accounts);
     });
 
     it('should show pending Zorro rewards for single tranche', async () => {
@@ -96,26 +89,18 @@ contract('ZorroControllerAnalytics', async accounts => {
         const zorPerBlock = await instance.ZORROPerBlock.call();
         const poolAllocPoints = web3.utils.toBN(1);
         const totAllocPoints = web3.utils.toBN(1);
-
+    
         const accRewards = elapsedBlocks.mul(zorPerBlock).mul(poolAllocPoints).div(totAllocPoints);
         const totalContribs = (await instance.poolInfo.call(0)).totalTrancheContributions;
         const expPendingRewards = web3.utils.toBN(contribution).mul(accRewards).div(totalContribs);
-
+    
         const pendingRewards = await instance.pendingZORRORewards.call(
             0, // pid
             accounts[1], 
             0 // trancheId
         );
-
+    
         assert.isTrue(pendingRewards.eq(expPendingRewards));
-    });
-});
-
-contract('ZorroControllerAnalytics', async accounts => {
-    let instance;
-
-    before(async () => {
-        instance = await getInstance(accounts);
     });
 
     it('should show zero pending Zorro rewards for exited tranche', async () => {
@@ -137,22 +122,16 @@ contract('ZorroControllerAnalytics', async accounts => {
         const pendingRewards = await instance.pendingZORRORewards.call(
             0, // pid
             accounts[1], 
-            0 // trancheId
+            1 // trancheId
         );
 
         assert.equal(pendingRewards, 0);
     });
-});
 
-contract('ZorroControllerAnalytics', async accounts => {
-    const contribution = web3.utils.toWei('1', 'ether');
-    let instance;
-    let contribs = web3.utils.toBN(0);
-
-    before(async () => {
-        // Get instance 
-        instance = await getInstance(accounts);
-
+    it('should show pending Zorro rewards for all tranches', async () => {
+        // Prep
+        const contribution = web3.utils.toWei('1', 'ether');
+        let contribs = web3.utils.toBN(0);
         // Add tranches
         for (let i = 0; i < 2; i++) {
             await instance.addTranche(
@@ -169,23 +148,17 @@ contract('ZorroControllerAnalytics', async accounts => {
             );
             contribs = contribs.add(web3.utils.toBN(contribution));
         }
-    });
-
-    it('should show pending Zorro rewards for all tranches', async () => {
-        // Prep
+        
+        // Vars
         const totalContribs = web3.utils.toBN((await instance.poolInfo.call(0)).totalTrancheContributions);
         const poolInfo = await instance.poolInfo.call(0);
         const elapsedBlocks = web3.utils.toBN(await web3.eth.getBlockNumber()).sub(poolInfo.lastRewardBlock);
         const zorPerBlock = await instance.ZORROPerBlock.call();
-        console.log('elapsedBlocks: ', elapsedBlocks.toString());
-        console.log('zorPerBlock: ', zorPerBlock.toString());
+
         const poolAllocPoints = web3.utils.toBN(1);
         const totAllocPoints = web3.utils.toBN(1);
 
         const accRewards = elapsedBlocks.mul(zorPerBlock).mul(poolAllocPoints).div(totAllocPoints);
-        console.log('contribs: ', contribs.toString());
-        console.log('accRewards: ', accRewards.toString());
-        console.log('totalContribs: ', totalContribs.toString());
         const expPendingRewards = contribs.mul(accRewards).div(totalContribs);
 
         // Run
@@ -198,8 +171,19 @@ contract('ZorroControllerAnalytics', async accounts => {
         // Tests
         console.log('pendingRewards: ', pendingRewards.toString());
         console.log('expPendingRewards: ', expPendingRewards.toString());
-        assert.isTrue(pendingRewards.eq(expPendingRewards));
+        assert.closeTo(pendingRewards.div(web3.utils.toBN(1e18)).toNumber(), expPendingRewards.div(web3.utils.toBN(1e18)).toNumber(), 1);
     });
+});
+
+contract('ZorroControllerAnalytics::Staked Want tokens', async accounts => {
+    let instance;
+    
+    before(async () => {
+        // Get instance 
+        instance = await (await setupObj(accounts)).instance;
+    });
+    
+    
 
     it('should show the amt Want tokens staked for a single tranche', async () => {
         // Prep
