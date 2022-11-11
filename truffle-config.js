58c0ec390035ba3af5e19c92700d83158b1b9d0f
Deltakilomilo <deltakilomilo@zorro.finance>
2022-11-11 08:43:26 -0500

    truffle upgrade

--- truffle-config.js
+++ truffle-config.js
@@ -1,33 +1,56 @@
 /**
  * Use this file to configure your truffle project. It's seeded with some
  * common settings for different networks and features like migrations,
- * compilation and testing. Uncomment the ones you need or modify
+ * compilation, and testing. Uncomment the ones you need or modify
  * them to suit your project as necessary.
  *
  * More information about configuration can be found at:
  *
- * trufflesuite.com/docs/advanced/configuration
+ * https://trufflesuite.com/docs/truffle/reference/configuration
  *
- * To deploy via Infura you'll need a wallet provider (like @truffle/hdwallet-provider)
- * to sign your transactions before they're sent to a remote public node. Infura accounts
- * are available for free at: infura.io/register.
+ * Hands-off deployment with Infura
+ * --------------------------------
  *
- * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. If you're publishing your code to GitHub make sure you load this
- * phrase from a file you've .gitignored so it doesn't accidentally become public.
+ * Do you have a complex application that requires lots of transactions to deploy?
+ * Use this approach to make deployment a breeze 🏖️:
  *
+ * Infura deployment needs a wallet provider (like @truffle/hdwallet-provider)
+ * to sign transactions before they're sent to a remote public node.
+ * Infura accounts are available for free at 🔍: https://infura.io/register
+ *
+ * You'll need a mnemonic - the twelve word phrase the wallet uses to generate
+ * public/private key pairs. You can store your secrets 🤐 in a .env file.
+ * In your project root, run `$ npm install dotenv`.
+ * Create .env (which should be .gitignored) and declare your MNEMONIC
+ * and Infura PROJECT_ID variables inside.
+ * For example, your .env file will have the following structure:
+ *
+ * MNEMONIC = <Your 12 phrase mnemonic>
+ * PROJECT_ID = <Your Infura project id>
+ *
+ * Deployment with Truffle Dashboard (Recommended for best security practice)
+ * --------------------------------------------------------------------------
+ *
+ * Are you concerned about security and minimizing rekt status 🤔?
+ * Use this method for best security:
+ *
+ * Truffle Dashboard lets you review transactions in detail, and leverages
+ * MetaMask for signing, so there's no need to copy-paste your mnemonic.
+ * More details can be found at 🔎:
+ *
+ * https://trufflesuite.com/docs/truffle/getting-started/using-the-truffle-dashboard/
  */
 
+// require('dotenv').config();
+// const { MNEMONIC, PROJECT_ID } = process.env;
+
 // const HDWalletProvider = require('@truffle/hdwallet-provider');
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   /**
    * Networks define how you connect to your ethereum client and let you set the
    * defaults web3 uses to send transactions. If you don't specify one truffle
-   * will spin up a development blockchain for you on port 9545 when you
+   * will spin up a managed Ganache instance for you on port 9545 when you
    * run `develop` or `test`. You can ask a truffle command to use a specific
    * network from the command line, e.g
    *
@@ -37,47 +60,48 @@ module.exports = {
   networks: {
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
+    // You should run a client (like ganache, geth, or parity) in a separate terminal
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
-    //
     ganachecli: {
-     host: "127.0.0.1",     // Localhost (default: none)
-     port: 8545,            // Standard Ethereum port (default: none)
-     network_id: "*",       // Any network (default: none)
-    },
-    avaxfork: {
-      url: process.env.GANACHE_CLOUD_URL,
-      network_id: 43114,
-    },
-    bscfork: {
-      url: process.env.GANACHE_CLOUD_URL,
-      network_id: 56,
-    },
-    // Another network with more advanced options...
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+     },
+     avaxfork: {
+       url: process.env.GANACHE_CLOUD_URL,
+       network_id: 43114,
+     },
+     bscfork: {
+       url: process.env.GANACHE_CLOUD_URL,
+       network_id: 56,
+     },
+
+    // An additional network, but with some advanced options…
     // advanced: {
-    // port: 8777,             // Custom port
-    // network_id: 1342,       // Custom network
-    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-    // from: <address>,        // Account to send txs from (default: accounts[0])
-    // websocket: true        // Enable EventEmitter interface for web3 (default: false)
+    //   port: 8777,             // Custom port
+    //   network_id: 1342,       // Custom network
+    //   gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
+    //   gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
+    //   from: <address>,        // Account to send transactions from (default: accounts[0])
+    //   websocket: true         // Enable EventEmitter interface for web3 (default: false)
     // },
+    //
     // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    // Note: It's important to wrap the provider as a function to ensure truffle uses a new provider every time.
+    // goerli: {
+    //   provider: () => new HDWalletProvider(MNEMONIC, `https://goerli.infura.io/v3/${PROJECT_ID}`),
+    //   network_id: 5,       // Goerli's id
+    //   confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
+    //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+    //   skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     // },
+    //
     // Useful for private networks
     // private: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-    // network_id: 2111,   // This network is yours, in the cloud.
-    // production: true    // Treats this network as if it was a public net. (default: false)
+    //   provider: () => new HDWalletProvider(MNEMONIC, `https://network.io`),
+    //   network_id: 2111,   // This network is yours, in the cloud.
+    //   production: true    // Treats this network as if it was a public net. (default: false)
     // }
     bsc: {
       provider: () => new HDWalletProvider(process.env.MNEMONIC_BSC, ''),
@@ -95,7 +119,7 @@ module.exports = {
     },
   },
 
-  // Set default mocha options here, use special reporters etc.
+  // Set default mocha options here, use special reporters, etc.
   mocha: {
     // timeout: 100000
   },
@@ -103,14 +127,14 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.10",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.10", // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       settings: {          // See the solidity docs for advice about optimization and evmVersion
        optimizer: {
          enabled: true,
          runs: 200
        },
-       evmVersion: "istanbul"
+       evmVersion: "byzantium"
       }
     }
   },
@@ -122,20 +146,19 @@ module.exports = {
   // NOTE: It is not possible to migrate your contracts to truffle DB and you should
   // make a backup of your artifacts to a safe location before enabling this feature.
   //
-  // After you backed up your artifacts you can utilize db by running migrate as follows: 
+  // After you backed up your artifacts you can utilize db by running migrate as follows:
   // $ truffle migrate --reset --compile-all
   //
   // db: {
-    // enabled: false,
-    // host: "127.0.0.1",
-    // adapter: {
-    //   name: "sqlite",
-    //   settings: {
-    //     directory: ".db"
-    //   }
-    // }
+  //   enabled: false,
+  //   host: "127.0.0.1",
+  //   adapter: {
+  //     name: "indexeddb",
+  //     settings: {
+  //       directory: ".db"
+  //     }
+  //   }
   // }
-
   plugins: [
     'truffle-contract-size',
     'truffle-plugin-verify'
