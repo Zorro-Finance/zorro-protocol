18e68feba4b5135ac64482b9e8f10ef707dd2109
Deltakilomilo <deltakilomilo@zorro.finance>
2023-01-11 21:16:41 -0500

    controller actions bdd

--- test/controllers/actions/ZorroControllerActions.js
+++ test/controllers/actions/ZorroControllerActions.js
@@ -4,34 +4,45 @@
 contract('ZorroControllerActions :: Investments', async accounts => {
     it('Calculates rewards due and slashed rewards for a given tranche', async () => {
         /* GIVEN
+        - As a public user
         */
 
         /* WHEN
+        - When calling getAdjustedRewards()
         */
 
         /* THEN
+        - I expect to receive the correct number of rewards due and slashed rewards
         */
     });
 
     it('Calculates time multiplier factor for a given duration', async () => {
         /* GIVEN
+        - As a public user
         */
 
         /* WHEN
+        - When calling getTimeMultiplier
         */
 
         /* THEN
+        - I expect to recieve an amount equal to (1 + sqrt(num_weeks)) * 1e12
+        - I expect to receive simply 1e12 if the time multiplier is not active
         */
     });
 
     it('Calculate shares, adjusted for time multiplier', async () => {
         /* GIVEN
+        - As a public user
+        - A number of shares and a time multiplier value
         */
 
         /* WHEN
+        - I call getUserContribution()
         */
 
         /* THEN
+        - I expect to get an adjusted shares value of the shares times the time multiplier
         */
     });
 });
--- test/controllers/actions/ZorroControllerXChainActions.js
+++ test/controllers/actions/ZorroControllerXChainActions.js
@@ -4,23 +4,29 @@
 contract('ZorroControllerXChainActions :: Utilities', async accounts => {
     it('Converts raw bytes to an EVM address', async () => {
         /* GIVEN
+        - A raw bytes encoded EVM address
         */
 
         /* WHEN
+        - Calling bytesToAddress()
         */
 
         /* THEN
+        - I should receive a valid address equivalent to what I put in
         */
     });
 
     it('Strips the function signature from a raw bytes payload with signature', async () => {
         /* GIVEN
+        - An ABI encoded payload with function signature
         */
 
         /* WHEN
+        - Calling extractParamsPayload()
         */
 
         /* THEN
+        - I should receive a bytes array with the signature removed (i.e. just the payload)
         */
     });
 });
@@ -28,23 +34,29 @@ contract('ZorroControllerXChainActions :: Utilities', async accounts => {
 contract('ZorroControllerXChainActions :: Deposits', async accounts => {
     it('Checks with Stargate to determine how much fees a deposit will cost', async () => {
         /* GIVEN
+        - A destination contract, chain, and payload
         */
 
         /* WHEN
+        - Calling checkXChainDepositFee()
         */
 
         /* THEN
+        - I should receive the fee required to make the deposit
         */
     });
 
     it('Encodes cross-chain deposit instructions into a bytes payload', async () => {
         /* GIVEN
+        - Vault parameters
         */
 
         /* WHEN
+        - Calling encodeXChainDepositPayload()
         */
 
         /* THEN
+        - A valid bytes payload is calculated
         */
     });
 });
@@ -52,34 +64,45 @@ contract('ZorroControllerXChainActions :: Deposits', async accounts => {
 contract('ZorroControllerXChainActions :: EarningsDistribution', async accounts => {
     it('Checks with Stargate to determine how much fees an earnings distribution will cost', async () => {
         /* GIVEN
+        - A destination contract, chain, and payload
         */
 
         /* WHEN
+        - Calling checkXChainDistributeEarningsFee()
         */
 
         /* THEN
+        - I should receive the fee required to make the earnings distribution
         */
     });
 
     it('Encodes cross-chain earnings distribution instructions into a bytes payload', async () => {
         /* GIVEN
+        - Vault parameters
         */
 
         /* WHEN
+        - Calling encodeXChainDistributeEarningsPayload()
         */
 
         /* THEN
+        - A valid bytes payload is calculated
         */
     });
 
     it('Receives and fulfills an earnings distribution request', async accounts => {
         /* GIVEN
+        - Buyback and revshare parameters
         */
 
         /* WHEN
+        - Calling distributeEarnings()
         */
 
         /* THEN
+        - Funds are transferred from the sender
+        - On chain buyback is executed for the correct amount
+        - On chain revshare is executed for the correct amount
         */
 
         // TODO: Expect buyback, revshare, etc.
@@ -89,62 +112,47 @@ contract('ZorroControllerXChainActions :: EarningsDistribution', async accounts 
 contract('ZorroControllerXChainActions :: Withdrawals', async accounts => {
     it('Checks with Stargate to determine how much fees a withdrawal will cost', async () => {
         /* GIVEN
+        - A destination contract, chain, and payload
         */
 
         /* WHEN
+        - Calling checkXChainWithdrawalFee()
         */
 
         /* THEN
+        - I should receive the fee required to make the withdrawal
         */
     });
 
     it('Encodes cross-chain withdrawal instructions into a bytes payload', async () => {
         /* GIVEN
+        - Trache and account parameters
         */
 
         /* WHEN
+        - Calling encodeXChainWithdrawalPayload()
         */
 
         /* THEN
+        - Receive a valid bytes payload
         */
     });
 
     it('Checks with Stargate to determine how much fees a repatriation will cost', async () => {
         /* GIVEN
+        - A destination contract and payload
         */
 
         /* WHEN
+        - Calling checkXChainRepatriationFee()
         */
 
         /* THEN
+        - A fee from Stargate is obtained
         */
     });
 
     it('Encodes cross-chain repatriation instructions into a bytes payload', async () => {
-        /* GIVEN
-        */
-
-        /* WHEN
-        */
-
-        /* THEN
-        */
-    });
-});
-
-contract('ZorroControllerXChainActions :: Withdrawals', async accounts => {
-    it('Performs a swap safely', async () => {
-        /* GIVEN
-        */
-
-        /* WHEN
-        */
-
-        /* THEN
-        */
-    });
-
-    it('Adds liquidity', async () => {
         /* GIVEN
         */
 
